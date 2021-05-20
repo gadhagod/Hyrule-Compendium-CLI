bad533fdad108530f638d90fbaaf76fab3094b48
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-05-19 22:02:25 -0700

    Bump pyrule-compendium to 2.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='hyrule-compendium-cli',
-    version='2.0.0',
+    version='3.0.1',
     author='Aarav Borthakur',
     author_email='gadhaguy13@gmail.com',
     description='The official CLI for the Hyrule Compendium API',
@@ -16,7 +16,7 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     install_requires=[
-        'pyrule-compendium==2.3.0',
+        'pyrule-compendium==2.4.0',
         'click',
         'Pygments',
         'termcolor'
